0f5a5ef22171875226e687fdbc478033128573c7
Oleksii Borshch <sergeevi44@gmail.com>
2022-06-08 03:38:34 +0300

    database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import './sass/main.scss';
+import './css/main.scss';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import { getDatabase, ref, set, onValue, get } from 'firebase/database';
+import * as firebaseui from 'firebaseui';
+import 'firebaseui/dist/firebaseui.css';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+
+const refs = {
+  form: document.querySelector('.form'),
+  submitBtn: document.querySelector('button'),
+  name: document.querySelector('input[name="name"]'),
+  pass: document.querySelector('input[name="password"]'),
+};
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,7 +26,33 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-console.log(app);
-console.log(db);
+// console.log(app);
+// console.log(db);
+
+refs.submitBtn.addEventListener('click', onSubmitBtnClick);
+
+function writeData(id, name, pass, movies) {
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  const userData = ref(db, `User${id}Info`);
+
+  set(userData, {
+    userid: id,
+    username: name,
+    password: pass,
+    movies,
+  });
+  onValue(userData, snapshot => {
+    const data = snapshot.val();
+    console.log(data);
+  });
+}
+
+function onSubmitBtnClick(e) {
+  e.preventDefault();
+  const name = refs.name.value;
+  const pass = refs.pass.value;
+  const id = 1;
+  const movies = [1, 2, 3, 4, 5];
+  writeData(id, name, pass, movies);
+}
